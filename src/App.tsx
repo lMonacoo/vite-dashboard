bf6c8aa7e762b82582624a4f7c3beed46d083a97
lMonacoo <lucas.monaco.maquina@gmail.com>
2022-03-01 07:14:09 -0300

    🎉: config the project to start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Text = styled.p`
+  font-size: 30px;
+`;
+
+function App() {
+  return (
+    <div className='App'>
+      <Text>🪂 Vite Docs</Text>
+    </div>
+  );
+}
+
+export default App;
